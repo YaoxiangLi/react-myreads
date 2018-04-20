1222a348b4b935fe7550104b1273a41a4bff884b
Yaoxiang Li <liyaoxiang@outlook.com>
2018-04-19 23:55:59 -0400

    updated booklist

--- src/BookList.js
+++ src/BookList.js
@@ -1,38 +1,20 @@
-import React, { Component } from 'react'
-import BookShelf from './BookShelf'
+import React from 'react'
+import Book from 'Book'
 
-class BookList extends Component {
+class BookList extends React.Component {
   render() {
-    const books = this.props.books
-    const shelfs = [
-      {
-        name: "currentlyReading",
-        title: "Currently Reading"
-      },
-      {
-        name: "wantToRead",
-        title: "Want to Read"
-      },
-      {
-        name: "read",
-        title: "Read"
-      },
-    ]
     return (
-      <div className="list-books-content">
-        <div>
-          {shelfs.map((shelf, index) => (
-            <BookShelf
-              key={ index }
-              title={ shelf.title }
-              books={ books.filter((book) => book.shelf === shelf.name)}
-              onShelfChange={(id, shelf) => {
-                this.props.onShelfChange(id, shelf)
-              }}
+      <ol className="books-grid">
+        {
+          this.props.list.map((book) =>
+            <Book
+              key={book.id}
+              book={book}
+              handleShelfUpdate={this.props.handleShelfUpdate}
             />
-          ))}
-        </div>
-      </div>
+          )
+        }
+      </ol>
     )
   }
 }
