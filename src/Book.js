54d63202f245e38aa46eccd0663356753f1a7913
Yaoxiang Li <liyaoxiang@outlook.com>
2018-04-19 23:55:47 -0400

    updated book

--- src/Book.js
+++ src/Book.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Book extends Component {
+class Book extends React.Component {
   render() {
+    const { book: { imageLinks, title, authors, shelf }, handleShelfUpdate } = this.props;
+    const thumbnail = imageLinks && imageLinks.thumbnail ? imageLinks.thumbnail : null;
     return (
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{
-                width: 128,
-                height: 193,
-                backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+            <div className="book-cover" style={{ backgroundImage: `url('${thumbnail}')`}}></div>
             <div className="book-shelf-changer">
-              <select>
-                <option value="none" disabled>Move to...</option>
+              <select value={shelf || 'none'} onChange={(event => handleShelfUpdate(this.props.book, event.target.value))}>
+                <option value="na" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
                 <option value="read">Read</option>
@@ -20,14 +19,12 @@ class Book extends Component {
               </select>
             </div>
           </div>
-          <div className="book-title">To Kill a Mockingbird</div>
-          <div className="book-authors">Harper Lee</div>
+          <div className="book-title">{title}</div>
+          <div className="book-authors">{!!authors && authors.join(', ')}</div>
         </div>
       </li>
     )
-
   }
 }
-
 
 export default Book;
